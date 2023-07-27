1caa5f1e5e26ea26ac207e4271f50d2d0c242612
White Hand <White Hand@DESKTOP-8O2Q7AJ>
2023-07-27 21:40:20 +0300

    create some animation using TransitionGroup add connection to jsonplaceholder server add axios package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useMemo, useRef,useEffect } from 'react';
 import  { useState } from 'react';
+import PostService from './API/PostService';
 import Counter from './components/Counter';
 import PostFilter from './components/PostFilter';
 import PostFormCreator from './components/PostFormCreator';
@@ -7,8 +8,11 @@ import PostItem from './components/PostItem';
 import PostList from './components/PostList';
 import ButtonInput from './components/UI/buttons/ButtonInput';
 import MyInput from './components/UI/inputs/MyInput';
+import Loader from './components/UI/loader/Loader';
 import MyModal from './components/UI/modalWindow/MyModal';
 import MySelect from './components/UI/select/MySelect';
+import { useFetching } from './hooks/useFetching';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css'
 
 
@@ -23,6 +27,21 @@ function App() {
   // const bodyRef=useRef();
   const [filter,setFilter]=useState({sort:'',query:''});
   const [modal,setModal]=useState(false);
+  const [totalCount,setTotalCount]=useState(0);
+  const [limit,setLimit]=useState(10);
+  const [page,setPage]=useState(1);
+  const sortedAnsSearchedPosts=usePosts(posts,filter.sort,filter.query);
+    // Добавить пакет аксиос и транзитион групп плюс ссылку на ресурс
+
+  const [fetchPosts,isLoadingPostPage,postError]=useFetching(async ()=>{
+    const response=await PostService.getPostsFromJSON(limit,page);
+    setPosts(response.data);    
+    setTotalCount(response.headers['x-total-count'])
+  })
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   function removePost(post){
   setPosts(posts.filter(p=>p.id!==post.id))
@@ -33,16 +52,7 @@ function App() {
     setModal(false);
   }
 
-const sortedPosts=useMemo(()=>{
-  if (filter.sort){
-    return [...posts].sort((a,b)=>a[filter.sort].localeCompare(b[filter.sort]));
-  }
-  return posts;
-}, [filter.sort,posts]);
 
-const sortAndSearchPosts=useMemo(()=>{
-return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query))
-},[filter.query,sortedPosts])
 
   return (
     <div className="App">
@@ -55,14 +65,20 @@ return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query))
     <MyModal visible={modal} setVisible={setModal} >
         <PostFormCreator  create={createPost}/>
     </MyModal>
-    {/* <hr style={{margin:'15px 0px'}}></hr> */}
 
    <PostFilter
      filter={filter}
      setFilter={setFilter}
    /> 
    
-    <PostList remove={removePost} posts={sortAndSearchPosts} title='List  of posts'/>
+    {postError && <h1>Error {postError}</h1>}
+
+    {isLoadingPostPage
+    ?
+    <div style={{display:'flex', justifyContent:'center', marginTop:'50px'}}> <Loader/></div>
+    :
+    <PostList remove={removePost} posts={sortedAnsSearchedPosts} title='List  of posts'/>
+    }
        
     </div>
   );
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PostItem from './PostItem';
 
 
@@ -12,9 +13,17 @@ const PostList =({posts,title,remove})=>{
     return(
         <div>
             <h1 style={{textAlign:'center'}}>{title}</h1>
+            <TransitionGroup>
             {posts.map((post,index)=>
+            <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post"
+            >
             <PostItem remove={remove} number={index+1} post={post} key={post.id}/>
+            </CSSTransition>
             )}
+            </TransitionGroup>
         </div>
     )
 }
